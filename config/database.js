b7d35274292ce4b29345bbc3b89a0c0542796b42
Caitlin McArdle <cait.e.mcardle@gmail.com>
2020-06-22 13:02:26 +0100

    database setup

--- config/database.js
+++ config/database.js
@@ -1,15 +1,26 @@
-module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+const url = require("url");
+let settings = {
+  client: process.env.NODE_ENV === "production" ? "postgres" : "sqlite3",
+};
+if (process.env.DATABASE_URL) {
+  const parsed = url.parse(process.env.DATABASE_URL, true);
+  const [username, password] = parsed.auth.split(":");
+  settings.host = parsed.hostname;
+  settings.port = Number(parsed.port);
+  settings.database = parsed.pathname.substr(1);
+  settings.username = username;
+  settings.password = password;
+  settings.ssl = {
+    rejectUnauthorized: false,
+  };
+}
+module.exports = {
+  defaultConnection: "default",
   connections: {
     default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
-      },
-      options: {
-        useNullAsDefault: true,
-      },
+      connector: "bookshelf",
+      settings,
+      options: {},
     },
   },
-});
+};
